cb635e32154f965b5093ec4543501edb8e9d822c
Christopher Brown <io@henrian.com>
2015-11-22 17:58:48 -0600

    Remove type_declarations/

--- type_declarations/index.d.ts
+++
@@ -1,4 +0,0 @@
-/// <reference path="DefinitelyTyped/htmlparser2/htmlparser2.d.ts" />
-/// <reference path="DefinitelyTyped/mocha/mocha.d.ts" />
-/// <reference path="DefinitelyTyped/node/node.d.ts" />
-/// <reference path="DefinitelyTyped/request/request.d.ts" />
